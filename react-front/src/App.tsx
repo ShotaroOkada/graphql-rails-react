dbb8f5d15165c2d7534ce42241e5bcecfc98a584
ShotaroOkada <b1016117@gmail.com>
2022-08-01 09:44:12 +0900

    [chore]graphql apollo

--- react-front/src/App.tsx
+++ react-front/src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from "@apollo/client";
+import { useBooksQuery } from "./graphql/generated";
 
 function App() {
+  const { data: { books = [] } = {} } = useBooksQuery();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {books.map((book) => {
+        return <div key={book.id}>{book.title}</div>;
+      })}
     </div>
   );
 }
