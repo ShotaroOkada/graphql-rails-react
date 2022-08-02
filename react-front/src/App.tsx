804aa379dd2d406a7ce8ac1a2ccea21962fbe875
ShotaroOkada <b1016117@gmail.com>
2022-08-02 09:18:31 +0900

    [add]book mutation

--- react-front/src/App.tsx
+++ react-front/src/App.tsx
@@ -1,13 +1,45 @@
-import { gql, useQuery } from "@apollo/client";
-import { useBooksQuery } from "./graphql/generated";
+import { useState } from "react";
+import {
+  useBooksQuery,
+  useCreateBookMutation,
+  useDeleteBookMutation,
+  useUpdateBookMutation,
+} from "./graphql/generated";
 
 function App() {
   const { data: { books = [] } = {} } = useBooksQuery();
+  const [createBook] = useCreateBookMutation({ refetchQueries: ["books"] });
+  const [title, setTitle] = useState("");
+  const [deleteBook] = useDeleteBookMutation({ refetchQueries: ["books"] });
+  const [updateBook] = useUpdateBookMutation();
 
   return (
     <div>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button
+        onClick={() => {
+          createBook({ variables: { params: { title: title } } });
+          setTitle("");
+        }}
+      >
+        保存
+      </button>
       {books.map((book) => {
-        return <div key={book.id}>{book.title}</div>;
+        return (
+          <div key={book.id}>
+            <input
+              value={book.title || ""}
+              onChange={(e) =>
+                updateBook({
+                  variables: { id: book.id, params: { title: e.target.value } },
+                })
+              }
+            />
+            <button onClick={() => deleteBook({ variables: { id: book.id } })}>
+              削除
+            </button>
+          </div>
+        );
       })}
     </div>
   );
